c328c478a1c278ae82d281ff54d10cfabd7dadc8
JaloDO <adiazsoftware@gmail.com>
2020-02-13 16:40:59 +0100

    project upload

---
+++ AdminFrutas/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace AdminFrutas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnNewClient = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flp1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.total = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnTicket = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Location = new System.Drawing.Point(577, 13);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(75, 23);
+            this.btnAdmin.TabIndex = 0;
+            this.btnAdmin.Text = "Administrar";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
+            // btnMod
+            // 
+            this.btnMod.Location = new System.Drawing.Point(577, 75);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(75, 23);
+            this.btnMod.TabIndex = 1;
+            this.btnMod.Text = "Modificar";
+            this.btnMod.UseVisualStyleBackColor = true;
+            this.btnMod.Visible = false;
+            this.btnMod.Click += new System.EventHandler(this.btnMod_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(577, 49);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = ' ';
+            this.txtPass.Size = new System.Drawing.Size(75, 20);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Visible = false;
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            // 
+            // btnNewClient
+            // 
+            this.btnNewClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewClient.Location = new System.Drawing.Point(615, 440);
+            this.btnNewClient.Name = "btnNewClient";
+            this.btnNewClient.Size = new System.Drawing.Size(86, 64);
+            this.btnNewClient.TabIndex = 3;
+            this.btnNewClient.Text = "Nuevo Cliente";
+            this.btnNewClient.UseVisualStyleBackColor = true;
+            this.btnNewClient.Click += new System.EventHandler(this.btnNewClient_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.BackgroundColor = System.Drawing.Color.Firebrick;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgv1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgv1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dgv1.Location = new System.Drawing.Point(13, 306);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(444, 214);
+            this.dgv1.TabIndex = 4;
+            this.dgv1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dgv1_MouseDoubleClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Precio (€)";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad (kg)";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Importe";
+            this.Column4.Name = "Column4";
+            // 
+            // flp1
+            // 
+            this.flp1.AutoScroll = true;
+            this.flp1.BackColor = System.Drawing.Color.Brown;
+            this.flp1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flp1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flp1.Location = new System.Drawing.Point(13, 13);
+            this.flp1.Name = "flp1";
+            this.flp1.Size = new System.Drawing.Size(558, 287);
+            this.flp1.TabIndex = 5;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 46F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total.ForeColor = System.Drawing.Color.Firebrick;
+            this.total.Location = new System.Drawing.Point(478, 331);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(64, 71);
+            this.total.TabIndex = 6;
+            this.total.Text = "0";
+            this.total.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnTicket
+            // 
+            this.btnTicket.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTicket.Location = new System.Drawing.Point(490, 440);
+            this.btnTicket.Name = "btnTicket";
+            this.btnTicket.Size = new System.Drawing.Size(90, 64);
+            this.btnTicket.TabIndex = 7;
+            this.btnTicket.Text = "Imprimir Ticket";
+            this.btnTicket.UseVisualStyleBackColor = true;
+            this.btnTicket.Click += new System.EventHandler(this.btnTicket_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LawnGreen;
+            this.ClientSize = new System.Drawing.Size(728, 526);
+            this.Controls.Add(this.btnTicket);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.flp1);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnNewClient);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.btnAdmin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnMod;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnNewClient;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.FlowLayoutPanel flp1;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnTicket;
+    }
+}
+
---
+++ AdminFrutas/Form1.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminFrutas
+{
+    public partial class Form1 : Form
+    {
+        ArrayList nombres=new ArrayList();
+        ArrayList precios = new ArrayList();
+        double p_total;
+        string codigo="frutas";
+
+        public Form1()
+        {
+            /*nombres = new String[]{ "chirimoya", "ciruela", "fresa", "kiwi", "mandarina", "melocoton", "melon",
+                "naranja", "nectarina", "papaya", "peras", "piña", 
+                "platanos", "pomelos", "prunus", "sandias","uvas" };
+
+            precios = new String[]{ "1,25", "2,75", "3,25", "1,50", "2,75", "3,25", "1",
+            "2", "3", "1", "2", "3", "1", "2", "3", "1", "2"}; */
+
+            
+            InitializeComponent();
+
+            
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarPrecios();
+            cargarBotones();
+
+        }
+        private void cargarPrecios()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int con = 0;
+                nombres.Clear();
+                precios.Clear();
+                String nombreFichero = openFileDialog1.FileName.ToString();
+                System.IO.StreamReader sr = new System.IO.StreamReader(nombreFichero);
+                while (sr.Peek() != -1)
+                {
+                    con++;
+                    String cadena = sr.ReadLine();
+                    String[] tramos = cadena.Split(':');
+                    nombres.Add(tramos[0]);
+                    precios.Add(tramos[1]);
+                    //MessageBox.Show("tramos-" + tramos[0] + "," + tramos[1] + "\nNombre y precio" + nombres[con] + "," + precios[con]);
+
+                }
+                sr.Close();
+            }
+        }
+
+        private void cargarBotones()
+        {
+            int cantidadBotones = nombres.Count;
+            Button botonX;
+            TextBox txtX;
+            for(int i = 0; i < cantidadBotones; i++)
+            {
+                botonX = new Button();
+                txtX = new TextBox();
+                txtX.Enabled = false;
+                txtX.Text = precios[i].ToString();
+
+                botonX.Tag = i;
+                
+                botonX.BackgroundImage = Image.FromFile(@"imagenes\" + nombres[i].ToString()+ ".PNG");
+                botonX.BackgroundImageLayout = ImageLayout.None;
+                botonX.Height=90;
+                botonX.Width = 90;
+                
+                
+                botonX.Click += new EventHandler(botonClick);
+
+                flp1.Controls.Add(botonX);
+                flp1.Controls.Add(txtX);
+            }
+        }
+        private void botonClick(Object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            String[] tupla ;
+
+            if (sender.GetType() == typeof(Button))
+            {try
+                {
+
+
+                    String cadena = Interaction.InputBox("Cantidad en kg", nombres[Convert.ToInt16(b.Tag)].ToString());
+                    tupla = new String[4]{
+                    nombres[Convert.ToInt16(b.Tag)].ToString(),
+                    precios[Convert.ToInt16(b.Tag)].ToString(),
+                    cadena,
+                   (Convert.ToDouble(cadena)*Convert.ToDouble(precios[Convert.ToInt16(b.Tag)])).ToString()};
+
+                    dgv1.Rows.Add(tupla);
+                    p_total += Convert.ToDouble(cadena) * Convert.ToDouble(precios[Convert.ToInt16(b.Tag)]);
+                    total.Text = Math.Round(p_total, 2) + " €";
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("ERROR CAZURRO", "title", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            txtPass.Visible = true;
+            btnMod.Visible = true;
+            txtPass.Focus();
+        }
+
+        private void btnNewClient_Click(object sender, EventArgs e)
+        {
+            p_total = 0;
+            total.Text = "0";
+            dgv1.Rows.Clear();
+
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        { 
+            TextBox t = (TextBox)sender;
+            if (t.Text.Equals(codigo)){
+                foreach(Control ctrl in flp1.Controls)
+                {   
+                    if (ctrl.GetType() == typeof(TextBox))
+                    {
+                        TextBox txt = (TextBox)ctrl;
+                        txt.Enabled = true;
+                    }
+                    else
+                    {
+                        Button b = (Button)ctrl;
+                        b.Enabled = false;
+                    }
+                }
+            }
+
+        }
+
+        private void btnMod_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+            foreach (Control ctrl in flp1.Controls)
+            {
+                if (ctrl.GetType() == typeof(TextBox))
+                {
+                    index++;
+                    TextBox txt = (TextBox)ctrl;
+                    txt.Enabled = false;
+                    precios[index] = txt.Text;
+
+                }
+                else
+                {
+                    Button b = (Button)ctrl;
+                    b.Enabled = true;
+                }
+            }
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String file = openFileDialog1.FileName.ToString();
+                actualizarFicheroPrecios(file);
+            }
+
+            txtPass.Visible = false;
+            btnMod.Visible = false;
+        }
+
+        private void actualizarFicheroPrecios(String file)
+        {
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(file);
+            for (int i = 0; i < precios.Count; i++)
+            {
+                String cadena = nombres[i] + ":" + precios[i];
+                sw.WriteLine(cadena);
+            }
+            sw.Close();
+        }
+
+        private void dgv1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            p_total -= Convert.ToDouble(dgv1.CurrentRow.Cells[3].Value);
+            total.Text =Math.Round(p_total,2)+" €";
+
+            dgv1.Rows.RemoveAt(dgv1.CurrentRow.Index);
+        }
+
+        private void btnTicket_Click(object sender, EventArgs e)
+        {
+            if (p_total > 0)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    String cad = "";
+                    String file = openFileDialog1.FileName.ToString();
+                    System.IO.StreamWriter sw = new System.IO.StreamWriter(file);
+                    for (int i = 0; i < dgv1.RowCount; i++)
+                    {
+                        cad = "Produto: " + dgv1.Rows[i].Cells[0].Value + "\tPrecio: " + dgv1.Rows[i].Cells[1].Value
+                             + "€\tCantidad: " + dgv1.Rows[i].Cells[2].Value + "Kg\tSubtotal: " + dgv1.Rows[i].Cells[3].Value + "€";
+                        sw.WriteLine(cad);
+
+                    }
+                    cad= "Importe Total: " + Math.Round(p_total, 2) + "€";
+                    sw.WriteLine(cad);
+                    sw.Close();
+
+                    p_total = 0;
+                    total.Text = "0";
+                    dgv1.Rows.Clear();
+                }
+                else MessageBox.Show("ERROR ocn el fichero");
+            }
+            else MessageBox.Show("NO has comprado NADA");
+        }
+    }
+}
